9b160659709508a3d6e9e2ae7245a213c5e2ceb4
realere <prettyere@gmail.com>
2016-01-22 17:07:25 +0000

    search and database works pretty well

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,8 @@
 require 'faker'
 include Faker
 
+location = ["Aberdeen", "Aberdeenshire", "Amber Valley", "Angus ", "Arun ", "Ashfield", "Aylesbury Vale", "BarnsleyTameside", "Basildon", "Basingstoke & Deane", "Bassetlaw", "Bath & North East Somerset", "Bedford", "Belfast", "Birmingham", "Blackburn", "Bolton", "Bournemouth", "Bracknell Forest", "Bradford", "Braintree", "Breckland", "Bridgend", "Brighton", "Bristol Kirklees", "Broadland", "Broxtowe", "Bury", "Caerphilly", "Calderdale", "Cambridge", "Canterbury", "Cardiff", "Carmarthenshire", "Charnwood", "Chelmsford", "Cheltenham", "Cherwell", "Chester", "Colchester", "Conway", "Coventry", "Crewe & Nantwich", "Dacorum ", "Derby", "Doncaster", "Dover ", "Dudley", "Dumfries & Galloway", "Dundee", "East Ayrshire", "East Devon ", "East Dunbartonshire ", "East Hampshire", "East Hertfordshire", "East Lindsey", "East Riding", "Eastleigh", "Edinburgh", "Elmbridge", "Epping Forest", "Erewash", "Exeter ", "Falkirk", "Fife", "Flintshire", "Gateshead", "Gedling", "Glasgow", "Gloucester", "Guildford", "Gwynedd", "Halton ", "Harrogate", "Havant & Waterloo", "Highland", "Horsham", "Huntingdonshire", "Ipswich", "Isle of Wight", "Kings Lynn & West Norfolk", "Kingston-upon-Hull", "Knowsley", "Lancaster", "Leeds", "Leicester", "LisburnScarborough", "Liverpool", "London", "Luton", "Macclesfield", "Maidstone", "Manchester", "Mid Bedfordshire ", "Mid Sussex ", "Middlesbrough", "Milton Keynes", "Neath Port Talbot", "New Forest", "Newbury", "Newcastle-under-Lyme", "Newcastle-upon-Tyne", "Newport", "North Ayrshire", "North East Lincolnshire", "North Hertfordshire", "North Lanarkshire", "North Lincolnshire", "North Tyneside", "North Wiltshire ", "Northampton", "Norwich", "Nottingham", "Nuneaton & Bedworth", "Oldham", "Oxford", "Pembrokeshire", "Perth & Kinross ", "Peterborough", "Plymouth", "Poole ", "Portsmouth", "Powys", "Preston ", "Reading", "Redcar & Clevelan", "Reigate & Banstead ", "Renfrewshire", "Rhondda, Cynon, Taff", "Rochdale", "Rochester-upon-Medway", "Rotherham", "Salford", "Salisbury ", "Sandwell", "Scottish Borders ", "Sefton", "Sevenoaks", "Sheffield", "Slough ", "Solihull", "South Ayrshire", "South Bedfordshire", "South Cambridgeshire", "South Gloucestershire", "South Kesteven", "South Lanarkshire", "South Oxfordshire", "South Somerset", "South Tyneside", "Southampton", "Southend-on-Sea", "St Albans", "St Helens", "Stafford", "Stockport", "Stockton-on-Tees", "Stoke-on-Trent", "Stratford-on-Avon", "Stroud", "Sunderland", "Swale", "Swansea", "Teignbridge", "Tendring ", "Test Valley", "Thamesdown", "Thanet", "The Wrekin", "Thurrock", "Torbay", "Trafford", "Vale of Glamorgan", "Vale of White Horse", "Wakefield", "Walsall", "WarringtonNorth Somerset", "Warwick ", "Waveney", "Waverley", "Wealden", "West Lancashire", "West Lothian", "West Wiltshire", "Wigan", "Windsor & Maidenhead", "Wirral", "Wokingham ", "Wolverhampton", "Wrexham Maelor", "Wychavon", "Wycombe", "York"]
 
-locations = %w( 'Aberdeen', 'Airdrie', 'Alloa', 'Arbroath', 'Ayr', 'Barrhead', 'Bathgate', 'Bearsden', 'Bellshill', 'Bishopbriggs', 'Blantyre', 'Bonnyrigg', 'Broxburn', 'Cambuslang', 'Clydebank', 'Coatbridge','Cumbernauld', 'Dumbarton', 'Dumfries', 'Dundee', 'Dunfermline', 'East Kilbride', 'Edinburgh', 'Elgin', 'Erskine' ,'Falkirk', 'Glasgow', 'Glenrothes', 'Grangemouth', 'Greenock', 'Hamilton', 'Inverness', 'Irvine', 'Johnstone', 'Kilmarnock', 'Kilwinning', 'Kirkcaldy and Dysart', 'Kirkintilloch', 'Larkhall', 'Livingston', 'Motherwell', 'Musselburgh', 'Newton Mearns', 'Paisley', 'Penicuik', 'Perth', 'Peterhead', 'Port Glasgow', 'Renfrew', 'Rutherglen', 'St Andrews', 'Stirling', 'Viewpark', 'Wishaw')
 prng = Random.new
 
 
@@ -26,13 +26,20 @@ prng = Random.new
   c = Childminder.new
   c.name = Faker::Name.name
    # c.childminder_image = Faker::Avatar.image "https://robohash.org/sitsequiquia.png?size=300x300"
-  c.no_of_children = prng.rand(3)+1
+  c.no_of_children = prng.rand(1..3)
   c.phone_number = Faker::PhoneNumber.cell_phone
   c.email = Faker::Internet.safe_email(c.name)
-  c.location = locations[rand(locations.length)]
-  c.rating = prng.rand(4)+1
+  c.location = location[rand(location.length)]
+  c.rating = prng.rand(3..5)
   c.about_me = Faker::Lorem.paragraph
   c.save
   puts "childminder #{c.name} created"
 end
+
+
+
+
+
+
+
 
