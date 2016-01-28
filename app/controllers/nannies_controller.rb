1875499c16554f53f4fe1706df991a762048ca12
realere <prettyere@gmail.com>
2016-01-28 09:04:36 +0000

    working ish

--- app/controllers/nannies_controller.rb
+++ app/controllers/nannies_controller.rb
@@ -72,6 +72,6 @@ class NanniesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def nanny_params
-      params.require(:nanny).permit(:name, :no_of_children, :phone_number, :email, :location, :rating, :about_me, :user_id)
+      params.require(:nanny).permit(:name, :no_of_children, :phone_number, :email, :location, :rating, :about_me, :user_id, :nanny_image, :remote_nanny_image_url)
     end
 end
